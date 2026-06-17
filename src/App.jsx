6d6e2a95d2b45016c49e227e96b297ade484e005
chandanatd495-ctd <chandanatd495@gmail.com>
2026-06-17 10:57:49 +0530

    Fixed routing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 import Home from "./Pages/Home";
 import About from "./Pages/About";
